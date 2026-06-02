324a2815137d7810c22db8e77541f1952c12be18
Swaraj Jinnad <42582124+Swaraj-jsk@users.noreply.github.com>
2026-06-02 17:12:20 +0530

    Add Google Tag Manager snippets

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,19 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en-IN" className={`${inter.variable} ${manrope.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KFK7V8JH');
+            `
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Base Meta Pixel, installed globally so every page view is tracked once. */}
         <script
           dangerouslySetInnerHTML={{
@@ -93,6 +106,16 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         </noscript>
       </head>
       <body style={{ fontFamily: "var(--font-inter)" }}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KFK7V8JH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(orgSchema) }}
