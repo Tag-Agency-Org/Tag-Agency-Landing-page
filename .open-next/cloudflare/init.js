822597dfd546e99cfdfd15e19dee1d0a6c4fbd26
Swaraj Jinnad <42582124+Swaraj-jsk@users.noreply.github.com>
2026-08-22 15:28:14 +0530

    Update OpenNext build artifacts

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1787384792783,
+    __BUILD_TIMESTAMP_MS__: 1787385140352,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
